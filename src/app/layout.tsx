1e1ce367ec2de7b6bd167a2e70f7565c890372a6
satyadev18 <satyaa1493@gmail.com>
2024-10-12 10:24:48 +0530

    added chatbot

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,41 +4,42 @@ import {
   Bangers,
   League_Spartan,
   Fredoka
-} from 'next/font/google';
-import './globals.css';
-import React from 'react';
-import { ReduxProvider } from '@/providers/ReduxProvider';
+} from "next/font/google";
+import "./globals.css";
+import React from "react";
+import Script from "next/script";
+import { ReduxProvider } from "@/providers/ReduxProvider";
 
 const inter = Inter({
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-inter'
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-inter"
 });
 
 const atkinson = Atkinson_Hyperlegible({
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-atkinson',
-  weight: '400'
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-atkinson",
+  weight: "400"
 });
 
 const bangers = Bangers({
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-bangers',
-  weight: '400'
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-bangers",
+  weight: "400"
 });
 
 const league = League_Spartan({
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-league'
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-league"
 });
 
 const fredoka = Fredoka({
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-fredoka'
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-fredoka"
 });
 
 export default function RootLayout({
@@ -51,6 +52,22 @@ export default function RootLayout({
       lang="en"
       className={`${inter.variable} ${atkinson.variable} ${bangers.variable} ${league.variable} ${fredoka.variable}`}
     >
+      <head>
+        <Script id="chatbot-config" strategy="beforeInteractive">
+          {`
+            window.embeddedChatbotConfig = {
+              chatbotId: "1ZYPlrW9E56Qhv7BVhwjH",
+              domain: "www.chatbase.co"
+            };
+          `}
+        </Script>
+
+        <Script
+          src="https://www.chatbase.co/embed.min.js"
+          strategy="afterInteractive"
+          defer
+        />
+      </head>
       <body>
         <ReduxProvider>{children}</ReduxProvider>
       </body>
