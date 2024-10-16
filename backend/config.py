7fd9bfc99ea168f00e5685773baf3bc94b1495e0
ajay kumar <ajayez0609@gmail.com>
2024-10-16 21:47:09 +0530

    hi

--- backend/config.py
+++ backend/config.py
@@ -1,7 +1,9 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:qwerty@localhost:5432/Digismile'
+    # SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:postgres@54.224.39.171:5432/digismile'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:postgres@54.224.39.171:5432/digismile'
+
     SQLALCHEMY_TRACK_MODIFICATION = False
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'your_jwt_secret_key')
     
