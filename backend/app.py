b11aa9fd12f488e13a592147176bd4f2978b5549
ajay kumar <ajayez0609@gmail.com>
2024-10-16 21:48:25 +0530

    hi

--- backend/app.py
+++
@@ -1,8 +0,0 @@
-# from flask_sqlalchemy import SQLAlchemy
-# from backend.config import Config
-from backend import create_app
-
-app = create_app()
-
-if __name__ == '__main__':
-    app.run(debug=False)
